35397f8c50697eb60eec49fc066d9cd841ada6d9
natal20-meet <natal20@meet.mit.edu>
2018-10-28 13:18:50 -0400

    lab4

---
+++ lab4.py
@@ -0,0 +1,27 @@
+#class animal (object):
+	def __init__(self,sound,name,age,favorite_color):
+#		self.sound = sound
+#		self.name = name
+#		self.age = age
+#		self.favorite_color = favorite_color
+#	def eat (self,food):
+#		print ("Yummy!! " +self.name + "is eating a " +food)
+#	def description(self):
+#		print (self.name + "is " + self.age + "years old and loves the color " +self.favorite_color)
+#	def make_sound (self):
+#		print ("oink!! "*3)	
+
+#pig = animal ("oink", "Robert ", "22 ", "red")
+#pig.eat("banana!")
+#pig.description ()
+#pig.make_sound ()
+
+class person (object):
+	def __init__(self, name, age, city, gender):
+		self.sound = sound
+		self.name = name
+		self.age = age
+		self.city = city
+		self.gender = gender
+
+perosna = person ("dolores", "68", "london", "male")	
